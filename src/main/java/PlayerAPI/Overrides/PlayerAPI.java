29e2ba886f7f3611df28aed898b8f22d266e7211
Steve Coulter <stevecoulter99@gmail.com>
2020-02-04 14:39:42 -0600

    add NetWorthImpl

--- src/main/java/PlayerAPI/Overrides/PlayerAPI.java
+++ src/main/java/PlayerAPI/Overrides/PlayerAPI.java
@@ -1,13 +1,63 @@
 package PlayerAPI.Overrides;
 
+import NetWorth.Overrides.NetWorthImpl;
+import NukkitDB.NukkitDB;
 import cn.nukkit.IPlayer;
 import cn.nukkit.Player;
 import cn.nukkit.network.SourceInterface;
 
-public class PlayerAPI extends Player implements IPlayer {
+public class PlayerAPI extends Player implements IPlayer, NetWorthImpl {
 
     public PlayerAPI(SourceInterface interfaz, Long clientID, String ip, int port) {
         super(interfaz, clientID, ip, port);
+    }
+
+    public int getMoney() {
+        String database = NetWorth.Main.getInstance().getConfig().getString("database");
+        String collection = NetWorth.Main.getInstance().getConfig().getString("collection");
+        return Integer.parseInt(
+                NukkitDB.query(
+                        getUuid(), "uuid", database, collection
+                ).get("balance").toString()
+        );
+    }
+
+    public void setMoney(int amount) {
+        String database = NetWorth.Main.getInstance().getConfig().getString("database");
+        String collection = NetWorth.Main.getInstance().getConfig().getString("collection");
+        NukkitDB.updateDocument(
+                getUuid(), "uuid", "balance", amount, database, collection
+        );
+    }
+
+    public void addMoney(int amount) {
+        String database = NetWorth.Main.getInstance().getConfig().getString("database");
+        String collection = NetWorth.Main.getInstance().getConfig().getString("collection");
+        int currentBalance = getMoney();
+        int newBalance = currentBalance + amount;
+        NukkitDB.updateDocument(
+                getUuid(), "uuid", "balance", newBalance, database, collection
+        );
+    }
+
+    public void subtractMoney(int amount) {
+        String database = NetWorth.Main.getInstance().getConfig().getString("database");
+        String collection = NetWorth.Main.getInstance().getConfig().getString("collection");
+        int currentBalance = getMoney();
+        int newBalance;
+        if (currentBalance <= 0) {
+            newBalance = 0;
+        } else {
+            newBalance = currentBalance - amount;
+        }
+        NukkitDB.updateDocument(
+                getUuid(), "uuid", "balance", newBalance, database, collection
+        );
+    }
+
+    @Override
+    public String getUuid() {
+        return this.getUniqueId().toString();
     }
 
     @Override
